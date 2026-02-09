adfef583410a2fe4c222818394677b46b935bc08
sajju061 <jackpaul123r@gmail.com>
2026-02-10 04:18:43 +0500

    Added heart animation

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ function next(){
   if(index >= cards.length) index = cards.length-1;
   cards[index].classList.add("active");
 }
-@keyframes heart {
-  0% { transform: scale(1) rotate(0deg); opacity:1; }
-  50% { transform: scale(1.3) rotate(15deg); opacity:0.7; }
-  100% { transform: scale(1) rotate(-15deg); opacity:1; }
-}
 
 document.body.addEventListener('click', e => {
   const heart = document.createElement('div');
