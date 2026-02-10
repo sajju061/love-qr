1bcfa62722dd4fd7c30fed16170de47bc54dcc9a
sajju061 <jackpaul123r@gmail.com>
2026-02-10 06:13:22 +0500

    Added proposal animation

--- script.js
+++ script.js
@@ -41,3 +41,39 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
 });
+
+
+
+
+
+
+
+
+let index = 0;
+let cards = document.querySelectorAll(".card");
+
+function next(){
+  cards[index].classList.remove("active");
+  index++;
+  if(index < cards.length){
+    cards[index].classList.add("active");
+  }
+}
+
+// ---- Proposal logic ----
+let yesCount = 0;
+let yesBtn = document.getElementById("yesBtn");
+
+function yesClick(){
+  yesCount++;
+  let scale = 1 + yesCount * 0.25;
+  yesBtn.style.transform = `scale(${scale})`;
+
+  if(yesCount >= 6){
+    next();
+  }
+}
+
+function noClick(){
+  alert("Dobara socho 😜❤️");
+}
